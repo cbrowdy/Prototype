2edcbf3be2c2393fb3f31bd49307bd8be74a9f42
LucCote <luccote@stmarksschool.org>
2017-11-23 19:19:23 -0500

    Created Help Page System
    Ditto

--- Create_Tables.php
+++ Create_Tables.php
@@ -8,4 +8,15 @@ if ($conn->query($sql) === TRUE) {
 } else {
     echo "Error creating user: " . $conn->error;
 }
+$sql = "CREATE TABLE Requests(
+    Question VARCHAR(255) NOT NULL,
+    Name VARCHAR(255) NOT NULL,
+    Email VARCHAR(255) NOT NULL,
+    Answered ENUM('true', 'false') DEFAULT 'false',
+    Satisfied ENUM('true', 'false') DEFAULT 'false');";
+if ($conn->query($sql) === TRUE) {
+    echo "created table";
+} else {
+    echo "Error creating user: " . $conn->error;
+}
 ?>
---
+++ Help_Request.php
@@ -0,0 +1,13 @@
+<?php
+require_once("DBConnect.php");
+$question = str_replace("'","",trim($_POST["Question"], " "));
+$name = str_replace("'","",trim($_POST["NAME"], " "));
+$email = str_replace("'","",trim($_POST["EMAIL"], " "));
+$sql = "INSERT INTO Requests (Question, Name, Email) VALUES ('".$question."', '".$name."', '".$email."')";
+if ($conn->query($sql) === TRUE) {
+    echo "request sucessfull";
+    header("Location: personalhelp.html");
+} else {
+    echo "Error submitting request: " . $conn->error;
+}
+?>
